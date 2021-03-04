8fdcf4fcd66d5ff1a1c4633a8baacd48f7793780
Yaping Liu <yaping.liu12@outlook.com>
2021-03-04 14:43:58 -0800

    finished task 7

--- index.js
+++ index.js
@@ -136,8 +136,9 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  let lastReview=`${array[array.length-1].name} gave the restaurant a ${array[array.length-1].rating} star review, and their feedback was: ${array[array.length-1].feedback}`;
+  return lastReview;
 } 
 
 
