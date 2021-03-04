befab01a8728dfa625bc03f910178fddf48bb5b3
Yaping Liu <yaping.liu12@outlook.com>
2021-03-04 13:27:52 -0800

    finished task 1a

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name,price,category){
+    return {name,price,category};
 }
+console.log(createMenuItem('tacos',8,'lunch'))
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
